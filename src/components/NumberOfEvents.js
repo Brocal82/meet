1da73044ef2d14d920b134d93af9ce21926c96ad
Brocal82 <antonioruizbrocal@gmail.com>
2023-08-11 12:53:04 +0200

    Create NumberOfEvents.js
     If applied, this commit will introduce the NumberOfEvents component in the src/components/NumberOfEvents.js file.
     If applied, this commit will improve the interaction of the NumberOfEvents component in the src/components/NumberOfEvents.js file. The changes optimize the input handling, making it easier for users to set the desired number of events to display.

---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,19 @@
+const NumberOfEvents = ({ setCurrentNOE }) => {
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    setCurrentNOE(value);
+  };
+
+  return (
+    <div id="number-of-events">
+      <input
+        type="text"
+        defaultValue="32"
+        onChange={handleInputChanged}
+        data-testid="numberOfEventsInput"
+      />
+    </div>
+  );
+};
+
+export default NumberOfEvents;
