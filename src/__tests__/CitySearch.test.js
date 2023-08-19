0929be1872e6a22492bcd81dca2e43d98e356212
Brocal82 <antonioruizbrocal@gmail.com>
2023-08-19 18:40:35 +0200

    Enhance <CitySearch /> Component Test
    If applied, this commit will improve the test suite for the <CitySearch /> component by adding test cases to simulate user interactions. The tests now cover scenarios where the user types 'Berlin' in the city textbox, utilizing the setInfoAlert and setCurrentCity functions for comprehensive testing.

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -9,7 +9,8 @@ import { extractLocations, getEvents } from '../api';
 describe('<CitySearch /> component', () => {
   let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch allLocations={[]}/>);
+    CitySearchComponent = render(<CitySearch allLocations={[]}
+    setInfoAlert={() => { }}/>);
   });
   test('renders text input', () => {
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -35,7 +36,8 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setInfoAlert={() => { }}/>
+    );
 
     // user types "Berlin" in city textbox
     const cityTextBox = CitySearchComponent.queryByRole('textbox'); 
@@ -58,7 +60,8 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents(); 
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setCurrentCity={() => { }} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setCurrentCity={() => { }} setInfoAlert={() => { }}/>
+    );
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
     await user.type(cityTextBox, "Berlin");
